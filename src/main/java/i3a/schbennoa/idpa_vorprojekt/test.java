2cec37f0faf7f7b42f49399b384148aeae14d052
Shenia Scherer <Shenia Scherer@172.17.25.139>
2020-08-19 15:25:44 +0200

    Test

---
+++ src/main/java/i3a/schbennoa/idpa_vorprojekt/test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package i3a.schbennoa.idpa_vorprojekt;
+
+/**
+ *
+ * @author Shenia Scherer
+ */
+public class test {
+    
+}
