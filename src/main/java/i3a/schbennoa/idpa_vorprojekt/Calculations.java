1a4266aa0491a22eb4190a41718f012cd06aae68
1810g <1810.grand@gmail.com>
2020-09-01 17:51:08 +0200

    Classes Calculations und Konto.java added

---
+++ src/main/java/i3a/schbennoa/idpa_vorprojekt/Calculations.java
@@ -0,0 +1,109 @@
+package i3a.schbennoa.idpa_vorprojekt;
+
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1810g
+ */
+public class Calculations {
+	double anschaffungswert=0;
+	double dauerInJahren=0;
+	double abschProzent=0;
+	double restwert=0;
+	int dirIn=0;
+	int liDeg=0;
+	Scanner in=new Scanner(System.in);
+	Konto[] konten=new Konto[3];
+
+
+	public Calculations(){
+		//Konto[0]=anlagevermoegen
+		//Konto[1]=WB Anlagevermoegen
+		//Konto[2]=abschreibungen
+		for(int i=0;i<3;i++){
+			konten[i]=new Konto();
+		}
+	
+	}
+	
+	public void startProgram(){
+		System.out.println("Geben sie bitte den Anschaffungswert ein");
+		anschaffungswert=Double.parseDouble(in.nextLine());
+
+		System.out.println("Geben sie bitte die Dauer in Jahren ein");
+		dauerInJahren=Double.parseDouble(in.nextLine());
+		
+		System.out.println("Wollen sie direkt(0) oder indirekt(1) verbuchen?");
+		dirIn=Integer.parseInt(in.nextLine());
+		
+		System.out.println("Wollen sie linear(0) oder degressiv(1) verbuchen?");
+		liDeg=Integer.parseInt(in.nextLine());
+		
+
+		//Befüllen von Anlagevermoegen Konto mit anschaffungswert
+		konten[0].add(anschaffungswert,1);
+		
+		if(liDeg==0){
+		//Lineare Abschreibung
+			System.out.println("Geben sie den Restwert ein");		
+			restwert=Double.parseDouble(in.nextLine());
+			double abschreibungsBetrag=((anschaffungswert-restwert)/dauerInJahren);	
+			for(int i=0;i<dauerInJahren;i++){
+			konten[2].setkontoStand(0);
+			konten[2].add(abschreibungsBetrag,1);	
+			konten[dirIn].add(abschreibungsBetrag, dirIn);
+			}	
+
+		
+		}else{
+		//Degressive Abschreibung	
+			System.out.println("Geben sie die Abschreibung pro Jahr in prozent an");
+			abschProzent=Double.parseDouble(in.nextLine());
+			double abschreibungsBetrag=0;
+			for(int i=0;i<dauerInJahren;i++){
+			//abschreibungsBetrag=konten[0].getkontoStand()*(abschProzent/100);
+			abschreibungsBetrag=(konten[0].getkontoStand()-konten[1].getkontoStand())*(abschProzent/100);
+			konten[2].setkontoStand(0);
+			konten[2].add(abschreibungsBetrag,1);	
+			konten[dirIn].add(abschreibungsBetrag,dirIn);
+			}
+		}
+		
+		
+		printOut();
+		
+
+
+		
+	}
+
+
+	private void printOut(){
+		System.out.println("Länge WB: "+konten[1].getBetraegeList().size());
+		for(int i=0;i<dauerInJahren;i++){
+			System.out.println("\n");
+			System.out.println("Abschreibungen: "+konten[2].getBetraegeList().get(i));
+			if(konten[0].getBetraegeList().size()==1){
+				//Arbeiten mit WB
+				System.out.println("WB Anlagevermoegen: "+konten[1].getBetraegeList().get(i));
+				System.out.println("Anlagevermoegen: "+konten[0].getkontoStand());
+			}else{
+				//Arbeiten mit Anlagevermoegen
+				System.out.println("Anlagevermoegen: "+konten[0].getBetraegeList().get(i));
+			}
+		}
+		System.out.println("\n Anlagevermögen am Ende: "+konten[0].getkontoStand());
+		
+	}
+
+	
+	
+}
